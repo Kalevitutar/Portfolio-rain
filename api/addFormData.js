9fc7b3d2d7d4ca2ce828bd147714b5e58286d08c
Kalevitutar <tanyamthomas@yahoo.com>
2024-07-23 12:49:32 -0400

    trying data-netlify true and handle onSubmit to see what happens

--- api/addFormData.js
+++ api/addFormData.js
@@ -1,22 +1,37 @@
 // import { API } from "aws-amplify";
 
 
-jQuery( document ).ready(function($) {
- $("#btnSubmit").on("click", function(e){
-    e.preventDefault();
-    let formData = {};
-        $("form :input").each(function(){
- let key = $(this)[0].name; // This is the jquery object of the input, do what you will
- let value = $(this)[0].value;
-console.log(key, value)
-if (key.length > 0) {
-    formData[key] = value;
-}
-});
-console.log(formData)
-    }); 
-});
+// jQuery( document ).ready(function($) {
+//  $("#btnSubmit").on("click", function(e){
+//     e.preventDefault();
+//     let formData = {};
+//         $("form :input").each(function(){
+//  let key = $(this)[0].name; // This is the jquery object of the input, do what you will
+//  let value = $(this)[0].value;
+// console.log(key, value)
+// if (key.length > 0) {
+//     formData[key] = value;
+// }
+// });
+// console.log(formData)
+//     }); 
+// });
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  console.log("formData: ", formData);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => navigate("/thank-you/"))
+    .catch((error) => alert(error));
+};
 
 
 
